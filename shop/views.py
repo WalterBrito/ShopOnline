17d902662457e02d0ea0a87e1f5bc76a37da1986
WalterBrito <waltter.britto@gmail.com>
2016-07-20 19:55:12 -0300

    Shop online made in django

--- shop/views.py
+++ shop/views.py
@@ -10,9 +10,10 @@ def product_list(request, category_slug=None):
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
-    return render(request, 'shop/product/list.html', {'category': category,
-                                                      'categories': categories,
-                                                      'products': products})
+    return render(request, 'shop/product/list.html',
+                  {'category': category,
+                   'categories': categories,
+                   'products': products})
 
 
 def product_detail(request, id, slug):
@@ -20,6 +21,6 @@ def product_detail(request, id, slug):
                                 id=id,
                                 slug=slug,
                                 available=True)
-    return render(render,
+    return render(request,
                   'shop/product/detail.html',
                   {'product': product})
