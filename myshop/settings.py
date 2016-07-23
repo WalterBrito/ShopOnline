678ba640317aab1e4bbbe3d3d13b1d275a7d807b
WalterBrito <waltter.britto@gmail.com>
2016-07-23 11:51:07 -0300

    Website shop online made in django

--- myshop/settings.py
+++ myshop/settings.py
@@ -109,3 +109,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 CART_SESSION_ID = 'cart'
+
+# tell Django to write emails to the console
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
