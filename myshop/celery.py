38c93b5cd02a17fbddc6bc3ac2e6b2b6d2bb67b3
WalterBrito <waltter.britto@gmail.com>
2017-05-25 17:22:41 -0300

    add celery and flower

--- myshop/celery.py
+++ myshop/celery.py
@@ -7,5 +7,5 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myshop.settings')
 
 app = Celery('myshop')
 
-app.config_from_object('django.conf.settings')
+app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
--- orders/tasks.py
+++ orders/tasks.py
@@ -8,13 +8,13 @@ def order_created(order_id):
     Task to send and e-mail notification when an order is 
     successfully created.
     """
-    order =  Order.objects.get(id=order_id)
+    order = Order.objects.get(id=order_id)
     subject = 'Order nr. {}'.format(order.id)
     message = 'Dear {}, \n\nYou have successfully placed an order.\
               Your order id is {}.'.format(order.first_name,
                                            order.id)
     mail_sent = send_mail(subject,
                           message,
-                          'jensen_johhanson@hotmail.com',
+                          'myshop@admin.com',
                           [order.email])
     return mail_sent
